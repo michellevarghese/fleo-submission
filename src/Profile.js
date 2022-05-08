2e4069a0fb484f6e1c7f072455236411eeec16d1
michellevarghese <michellemaryvarghese5@gmail.com>
2022-05-08 11:42:14 +0530

    submission

--- src/Profile.js
+++ src/Profile.js
@@ -2,24 +2,13 @@ import React from "react";
 
 const Profile = (Profile) => {
   const {html_url, name,description, owner,stargazers_count,forks_count,language  } = Profile;
-  //const { login, avatar_url, html_url } = Profile;
-
-//   return (
-//     <article className="card">
-//       <img src={avatar_url} alt={login} />
-//       <h4>{name}</h4>
-//       <a href={html_url} className="btn">
-//         View Profile
-//       </a>
-//     </article>
-//   );
-// };
+  
 
 
 return (
   <article className="card">
     
-    <h3>{name}</h3>
+    <h3 className="wrap-it">{name}</h3>
     <div id="myDesc">
     {description}
 </div>
@@ -33,25 +22,7 @@ return (
       View Profile
     </a>
   </article>
-//   <div class="ui card">
-//   <div class="image">
-//     <img src="/images/avatar2/large/kristy.png"/>
-//   </div>
-//   <div class="content">
-//     <a class="header">Kristy</a>
-//     <div class="meta">
-//       <span class="date">Joined in 2013</span>
-//     </div>
-//     <div class="description">
-//       {description}
-//     </div>
-//   </div>
-//   <div class="extra content">
-//   <a href={html_url} className="btn">
-//    View Profile
-//   </a>
-//   </div>
-// </div>
+
 
 );
 };
